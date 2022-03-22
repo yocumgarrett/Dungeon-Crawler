3f07713b46aac875fa63d352585327eab1432a8a
Garrett Yocum <yocumgarrett@gmail.com>
2022-03-22 10:07:35 -0700

    room prefabs with doors that control camera movement

---
+++ Assets/Scripts/Doors.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour
+{
+    public Transform CameraTransform;
+    public Camera mainCamera;
+    public Vector3 cameraOffset;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        mainCamera.transform.position = CameraTransform.position + cameraOffset;
+    }
+}

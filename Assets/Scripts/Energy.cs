a253679c901498b50d605ffc944aafee3f47d475
Garrett Yocum <yocumgarrett@gmail.com>
2022-05-01 13:25:06 -0700

    energy magnet

--- Assets/Scripts/Energy.cs
+++ Assets/Scripts/Energy.cs
@@ -6,11 +6,36 @@ using System;
 public class Energy : MonoBehaviour, ICollectible
 {
     public static event Action OnEnergyCollected;
+    Rigidbody2D rb;
+
+    bool hasTarget;
+    Vector3 targetPosition;
+    float moveSpeed = 4f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     public void Collect()
     {
         Debug.Log("energy collected");
         Destroy(gameObject);
         OnEnergyCollected?.Invoke();
+    }
+
+    private void FixedUpdate()
+    {
+        if (hasTarget)
+        {
+            Vector2 targetDirection = (targetPosition - transform.position).normalized;
+            rb.velocity = new Vector2(targetDirection.x, targetDirection.y) * moveSpeed;
+        }
+    }
+
+    public void SetTarget(Vector3 position)
+    {
+        targetPosition = position;
+        hasTarget = true;
     }
 }
---
+++ Assets/Scripts/Magnet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magnet : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<Energy>(out Energy energy))
+        {
+            energy.SetTarget(transform.parent.position);
+        }
+    }
+}

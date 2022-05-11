f514e611dbfc8ac137d6b74d28aad57ce0eea47b
Garrett Yocum <yocumgarrett@gmail.com>
2022-05-10 20:48:57 -0700

    enemy and attack gizmo

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int health;
+
+    private void Update()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        Debug.Log("oof!");
+        health -= damage;
+    }
+}
---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private float timeBetweenAttack;
+    public float startTimeBetweenAttack;
+
+    public Transform attackPos;
+    public LayerMask whatIsEnemies;
+    public float attackRange;
+    public int damage;
+
+    void Update()
+    {
+        if(timeBetweenAttack <= 0)
+        {
+            if (Input.GetKey(KeyCode.Z))
+            {
+                // create an array of enemies in the collider area that you can damage
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+                for(int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    //deal damage
+                    enemiesToDamage[i].GetComponent<EnemyHealth>().TakeDamage(damage);
+                }  
+            }
+            timeBetweenAttack = startTimeBetweenAttack;
+        }
+        else
+        {
+            timeBetweenAttack -= Time.deltaTime;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+}

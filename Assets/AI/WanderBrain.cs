b55188d8cd8fc112342a926410a6f837cbf37e21
Garrett Yocum <yocumgarrett@gmail.com>
2022-05-30 10:58:48 -0700

    wander brain

---
+++ Assets/AI/WanderBrain.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Brains/Wander")]
+public class WanderBrain : Brain
+{
+    private float timeBetweenUpdate = 0f;
+    public float startTimeBetweenUpdate;
+    public float distance;
+
+    private Vector2 movePosition;
+
+    public override void Think(EnemyThinker thinker)
+    {
+        if(timeBetweenUpdate <= 0)
+        {
+            Vector2 currentPosition = new Vector2(thinker.transform.position.x, thinker.transform.position.y);
+            //make a new random movement choice;
+            Vector2 randomDirection = new Vector2(2 * Random.value - 1, 2 * Random.value - 1);
+            randomDirection = distance * randomDirection.normalized;
+            movePosition = currentPosition + randomDirection;
+
+            
+            Debug.Log("new position: " + movePosition.x + " , " + movePosition.y);
+            timeBetweenUpdate = startTimeBetweenUpdate;
+        }
+        else
+        {
+            timeBetweenUpdate -= Time.deltaTime;
+        }
+
+        var movement = thinker.gameObject.GetComponent<Movement>();
+        if (movement)
+        {
+            movement.MoveTowardsTarget(movePosition);
+        }
+    }
+}

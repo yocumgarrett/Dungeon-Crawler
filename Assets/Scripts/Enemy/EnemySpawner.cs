dce5d3ea09503e436a5edf8368d4d4b5fa335644
Garrett Yocum <yocumgarrett@gmail.com>
2022-09-19 23:34:08 -0700

    enemy spawner, will make testing combat easier

---
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject EnemyPrefab;
+    public float timeBetweenSpawns;
+    private float spawnTimer = 0f;
+    private Vector3 spawnerPosition;
+
+    void Start()
+    {
+        spawnerPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if (spawnTimer >= timeBetweenSpawns)
+        {
+            SpawnEnemy();
+            spawnTimer = 0f;
+        }
+        else
+            spawnTimer += Time.deltaTime;
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(EnemyPrefab, transform.position, Quaternion.identity);
+    }
+}
